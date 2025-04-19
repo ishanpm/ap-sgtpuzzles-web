9aef4bb17600a4c319bce7eca1685d3c60b88059
Ishan Murphy <ishanpm@gmail.com>
2025-04-19 17:54:01 -0500

    Use relative import for production build

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,9 @@ import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import path from 'node:path'
 
-
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV == "production" ? "./" : "/",
   plugins: [
     vue(),
     vueDevTools(),
