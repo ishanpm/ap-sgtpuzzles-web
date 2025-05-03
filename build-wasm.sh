387dbed221690c69ed80d6e7b67a813153a816c7
Ishan Murphy <ishanpm@gmail.com>
2025-05-03 11:05:00 -0500

    Split archipelago build from original HTML build

--- build-wasm.sh
+++ build-wasm.sh
@@ -4,7 +4,7 @@ echo "Compiling with cmake"
 cd ap-sgtpuzzles
 set -e
 rm -f CMakeCache.txt
-emcmake cmake -B build-emscripten -DPUZZLES_ENABLE_UNFINISHED=group .
+emcmake cmake -B build-emscripten -DPUZZLES_ENABLE_UNFINISHED=group -DHTML_BUILD_MODE=archipelago .
 cd build-emscripten
 cmake --build .
 cd ../..
